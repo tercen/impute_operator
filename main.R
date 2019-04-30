d2f233887cd6192b6ed5286a96c1734d6334a88c
amaurel <alexandre.maurel@gmail.com>
2019-04-30 18:31:23 +0200

    merged

--- main.R
+++ main.R
@@ -6,7 +6,7 @@ matrix = ctx$as.matrix(fill=0.0)
 df = data.frame(
   imputed=as.vector(matrix),
   .ci=rep(0:(ncol(matrix)-1), each=nrow(matrix)),
-  .ri=rep(seq.int(from=0,to=nrow(data)-1), ncol(data)))
+  .ri=rep(seq.int(from=0,to=nrow(matrix)-1), ncol(matrix)))
  
 ctx$save(ctx$addNamespace(df))
   
