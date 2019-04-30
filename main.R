8ef5cda9b0598ea0a0430fdde05cea660081259d
amaurel <alexandre.maurel@gmail.com>
2019-04-30 17:33:42 +0200

    as.matrix

--- main.R
+++ main.R
@@ -1,15 +1,12 @@
 library(tercen)
-library(dplyr)
-library(reshape2)
 
-data = (ctx = tercenCtx()) %>% 
-  select(.ci, .ri, .y) %>% 
-  reshape2::acast(.ci ~ .ri, fill=0.0, value.var='.y', fun.aggregate=mean)
+ctx = tercenCtx()
+matrix = ctx$as.matrix(fill=0.0)
 
-data.frame(
-  .y=as.vector(data),
-  .ci=rep(seq.int(from=0,to=ncol(data)-1), nrow(data)),
-  .ri=rep(0:(nrow(data)-1), each=ncol(data))) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+df = data.frame(
+  .y=as.vector(matrix),
+  .ci=rep(seq.int(from=0,to=ncol(matrix)-1), nrow(matrix)),
+  .ri=rep(0:(nrow(matrix)-1), each=ncol(matrix)))
+ 
+ctx$save(ctx$addNamespace(df))
  
