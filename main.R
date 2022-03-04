11ea8393f0a2cacf3357964e5229cfc6affc23b2
lucaslelann <lucas.lelann@gmail.com>
2022-03-04 12:57:24 +0000

    Merge impute mean and impute

--- main.R
+++ main.R
@@ -1,13 +1,38 @@
 library(tercen)
- 
-ctx = tercenCtx()
+library(tidyr)
+library(dplyr)
+library(reshape2)
 
-matrix = ctx$as.matrix(fill=ctx$op.value("value"))
+ctx <- tercenCtx()
 
-df = data.frame(
-  imputed=as.vector(matrix),
-  .ci=rep(0:(ncol(matrix)-1), each=nrow(matrix)),
-  .ri=rep(seq.int(from=0,to=nrow(matrix)-1), ncol(matrix)))
- 
-ctx$save(ctx$addNamespace(df))
+if (ctx$op.value("mean")){
+  mat <- ctx %>%
+    select(.ci, .ri, .y) %>% 
+    reshape2::acast(.ri ~ .ci, value.var='.y', fill = NA) %>%
+    as.data.frame
   
+  mat[] <- lapply(mat, function(x) ifelse(is.na(x) | is.nan(x), mean(x, na.rm = TRUE), x)) 
+  colnames(mat) <- 1:ncol(mat) - 1
+  
+  mat_out <- mat %>%
+    mutate(.ri = 1:nrow(.) - 1) %>%
+    gather(.ci, mean_imputed, -.ri)              
+  
+  mat_out$.ci<- as.integer(mat_out$.ci)
+  mat_out$.ri<- as.integer(mat_out$.ri)
+} else{
+  
+  val<-ctx$op.value("value")
+  val<-1
+  
+  matrix <- ctx$as.matrix(fill=val)
+  
+  mat_out <- data.frame(
+    .ci=rep(0:(ncol(matrix)-1), each=nrow(matrix)),
+    .ri=rep(seq.int(from=0,to=nrow(matrix)-1), ncol(matrix)),
+    value_imputed=as.vector(matrix))
+}
+
+mat_out %>%
+  ctx$addNamespace()%>%
+  ctx$save()
